1bcc9a30546e6b9fb83a442180c0dac0793b39a1
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-12-08 19:17:40 +0530

    Updates

---
+++ precache-manifest.4c5de03814a870a1d7269292558612ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43123aa23a925a55103af5b9591a2d7d",
+    "url": "/Tool_Tip/index.html"
+  },
+  {
+    "revision": "57d7e673e87dff653143",
+    "url": "/Tool_Tip/static/css/main.b67bbe05.chunk.css"
+  },
+  {
+    "revision": "ffe6f151f4ba7d132ba0",
+    "url": "/Tool_Tip/static/js/2.2a4a0135.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tool_Tip/static/js/2.2a4a0135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d7e673e87dff653143",
+    "url": "/Tool_Tip/static/js/main.c794b600.chunk.js"
+  },
+  {
+    "revision": "7eda230c260cad364614",
+    "url": "/Tool_Tip/static/js/runtime-main.e17646be.js"
+  }
+]);
