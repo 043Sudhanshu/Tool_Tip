1f2615f80e892703394165463112f7631919ce81
sudhanshu chauhan <043sudhanshu@gmail.com>
2020-12-08 19:07:57 +0530

    deploy

--- src/Mainbutton.js
+++ src/Mainbutton.js
@@ -3,25 +3,29 @@ import React from 'react';
 function Mainbutton(props) {
    
   const pos=props.position;
-  console.log(pos); 
+  
   return (  
       <div className="Mainbutton" >
       { props.visible===true ?
        [ pos==="left" || pos==="right" ?
-       <div className="tip" style={{width:50,height:100,[pos]:-120}}>
-        Tip for u
-      </div>
-       :
-       <div className="tip" style={{[pos]:-120}}>
-          Tip for u
-         </div>
-       ]
+      
+          <div className="tip" style={{width:50,height:100,[pos]:-120}}>
+            Tip for u
+          </div>
+          
+          :
+          
+          <div className="tip" style={{[pos]:-120}}>
+              Tip for u
+            </div>
+      
+      ]
          :
          null
       
       }
        
-       {props.position}
+      <h2> {props.position.toUpperCase()} </h2>
       </div>
     )
   }
